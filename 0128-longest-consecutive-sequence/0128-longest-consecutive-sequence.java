c5a736b15d0bf6a424fa1ce8d44859511b4e741d
sagarrajput2111 <54945125+sagarrajput2111@users.noreply.github.com>
2023-06-29 14:50:21 +0530

    Time: 444 ms (22.15%), Space: 62.6 MB (13.92%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -4,35 +4,71 @@ class Solution {
         int n=nums.length;
         if(n==0)
             return 0;
-        Arrays.sort(nums);
-        int finalSum=1;
-        int tempSum=1;
+//         Arrays.sort(nums);
+//         int finalSum=1;
+//         int tempSum=1;
         
-        for(int i=0;i<n-1;i++)
-        {
+//         for(int i=0;i<n-1;i++)
+//         {
            
-            if(nums[i]==nums[i+1]-1)
+//             if(nums[i]==nums[i+1]-1)
+//             {
+//                 tempSum++;
+            
+            
+//             if(tempSum>finalSum)
+//                 finalSum=tempSum;
+//             }
+            
+//             else if(nums[i]==nums[i+1])
+//             {
+                
+//             }
+//             else
+//             {
+//                 tempSum=1;
+//             }
+                
+            
+            
+//         }
+//         return finalSum;
+        
+        //optimal approach
+        HashSet<Integer> set=new HashSet<>();
+        
+        for(int i=0;i<n;i++)
+        {
+            
+            set.add(nums[i]);
+        }
+        
+        int finalans=1;
+        int tempans=1;
+        
+        for(int i=0;i<n;i++)
+        {
+            tempans=1;
+            if(!set.contains(nums[i]-1))
             {
-                tempSum++;
-            
-            
-            if(tempSum>finalSum)
-                finalSum=tempSum;
-            }
-            
-            else if(nums[i]==nums[i+1])
-            {
+                int m=nums[i]+1;
+                while(set.contains(m))
+                {
+                    tempans++;
+                    m++;
+                }
                 
             }
-            else
-            {
-                tempSum=1;
-            }
-                
+            
+            if(finalans<tempans)
+                finalans=tempans;
+
             
             
         }
-        return finalSum;
         
+        
+        
+        return finalans;
     }
 }
