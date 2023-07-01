497146462282619173f62cfa04e53b195c3098a9
sagarrajput2111 <54945125+sagarrajput2111@users.noreply.github.com>
2023-07-01 19:58:35 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (61.13%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,15 +1,46 @@
 class Solution {
     public int search(int[] nums, int target) {
         
-        int pivot=findpivot(nums,0,nums.length-1);
-        if(pivot==0)
-            return bsearch(nums,0,nums.length-1,target);
-        int idx1=bsearch(nums,0,pivot-1,target);
-        if(idx1!=-1)
-            return idx1;
-        int idx2=bsearch(nums,pivot,nums.length-1,target);
-            return idx2;
+//         int pivot=findpivot(nums,0,nums.length-1);
+//         if(pivot==0)
+//             return bsearch(nums,0,nums.length-1,target);
+//         int idx1=bsearch(nums,0,pivot-1,target);
+//         if(idx1!=-1)
+//             return idx1;
+//         int idx2=bsearch(nums,pivot,nums.length-1,target);
+//             return idx2;
         
+        
+        int l=0,h=nums.length-1;
+        while(l<=h)
+        {
+            
+            int mid=l+(h-l)/2;
+            
+            if(nums[mid]==target)
+                return mid;
+            
+            if(nums[mid]>=nums[l])
+            {
+                //left side is sorted check if it present it or not
+                
+                if(target>=nums[l]&&target<nums[mid])
+                    h=mid-1;
+                else
+                    l=mid+1;
+                
+            }
+            
+            else
+            {
+                if(target<=nums[h]&&target>nums[mid])
+                    l=mid+1;
+                else
+                    h=mid-1;
+            }
+            
+        }
+        return -1;
         
     }
     
