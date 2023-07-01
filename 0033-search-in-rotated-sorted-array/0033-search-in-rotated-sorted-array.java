e14dd669229203504bc84c25cb342c490573d20f
sagarrajput2111 <54945125+sagarrajput2111@users.noreply.github.com>
2023-07-01 19:18:35 +0530

    Time: 0 ms (100.00%), Space: 41 MB (79.12%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,74 +1,53 @@
 class Solution {
+    public int search(int[] nums, int target) {
+        
+        int pivot=findpivot(nums,0,nums.length-1);
+        if(pivot==0)
+            return bsearch(nums,0,nums.length-1,target);
+        int idx1=bsearch(nums,0,pivot-1,target);
+        if(idx1!=-1)
+            return idx1;
+        int idx2=bsearch(nums,pivot,nums.length-1,target);
+            return idx2;
+        
+        
+    }
     
-    public int findP(int[] nums,int l, int h)
+    int bsearch(int[] nums,int l,int h,int target)
     {
         
         while(l<=h)
         {
             int mid=l+(h-l)/2;
-            if(nums[mid]<nums[0]&&nums[mid]<nums[mid-1])
-            {
-                return mid;
-            }
-            else if(nums[mid]>=nums[0])
-            {
-                l=mid+1;
-            }
-                
-            else if(nums[mid]<nums[0])
-            {
-            h=mid;
-            }
             
-        }
-        return -1;
-        
-    }
-    
-     public int bsearch(int[] nums, int target,int l,int h) {
-        
-        
-        while(l<=h)
-        {
-            int mid=l+(h-l)/2;
             if(nums[mid]==target)
                 return mid;
             else if(nums[mid]>target)
-            {
                 h=mid-1;
-            }
             else
+                l=mid+1;
+                
+        }
+        return -1;
+    }
+    
+    public int findpivot(int [] nums,int l , int  h)
+    {
+        while(l<h)
+        {
+            
+            int mid =l+(h-l)/2;
+            
+            if(nums[mid]>=nums[0])
             {
                 l=mid+1;
             }
+            else
+            {
+                h=mid;
+            }
         }
-        
-        return -1;
-        
-    }
-    public int search(int[] nums, int target) 
-    
-    
-        
-    {
-        
-    int pivot=findP(nums,0,nums.length-1);
-    System.out.println(pivot);
-       if(pivot==-1)
-           return bsearch(nums,target,0,nums.length-1);
-       int idx1=bsearch(nums,target,0,pivot-1);
-        int idx2=bsearch(nums,target,pivot,nums.length-1);
-        
-        System.out.println(idx1+" "+ idx2);
-        
-        if(idx1==-1&&idx2==-1)
-            return -1;
-        else if(idx1!=-1)
-            return idx1;
-        else
-            return idx2;
-        
-       
+        return h;
         
     }
 }
