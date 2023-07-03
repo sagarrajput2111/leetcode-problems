f59ecce38116fbb912af4596a8ca37439e074659
sagarrajput2111 <54945125+sagarrajput2111@users.noreply.github.com>
2023-07-03 16:11:22 +0530

    Time: 41 ms (77.14%), Space: 51.5 MB (26.04%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,50 +1,7 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] num) {
         
-//         Arrays.sort(nums);
-//         List<List<Integer>>list=new ArrayList<List<Integer>>();
-        
-        
-//         for(int i=0;i<nums.length-2;i++)
-//         {
-            
-//             if (i == 0 || (i > 0 && nums[i] != nums[i - 1])){
-            
-            
-//             //now we use two pointers to find the remainder
-            
-//             int l=i+i,h=nums.length-1,remainder=0-nums[i];
-//             while(l<h)
-//             {
-//                 if(nums[l]+nums[h]==remainder)
-//                 {
-//                                List<Integer> temp=new ArrayList<Integer>();
-
-                    
-//                     temp.add(nums[i]);
-//                     temp.add(nums[l]);
-//                     temp.add(nums[h]);
-//                     list.add(temp);
-                   
-//                      while (l < h && nums[l] == nums[l + 1]) l++;
-//                         while (l< h && nums[h] == nums[h - 1]) h--;
-
-//                          l++;
-//                         h--;
-//                      }
-                
-//                 else if (nums[l] + nums[h] < remainder) l++;
-
-//                      else h--;
-//             }
-//         }
-        
-//         }
-//        return list;
-        
-        
-        
-          Arrays.sort(num);
+ Arrays.sort(num);
 
         List<List<Integer>>res=new ArrayList<List<Integer>>();
 
@@ -75,6 +32,6 @@ class Solution {
         }
         return res;
         
-        
+                
     }
 }
