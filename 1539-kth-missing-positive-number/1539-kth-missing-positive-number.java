64e6c6092d37348ccf677276b9f7d294726d925b
sagarrajput2111 <54945125+sagarrajput2111@users.noreply.github.com>
2023-07-11 15:16:31 +0530

    Time: 0 ms (100.00%), Space: 42 MB (68.43%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -3,16 +3,44 @@ class Solution {
         
         //brute force
         
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]<k+1)
-                k++;
-            else
-                break;
+//         for(int i=0;i<arr.length;i++)
+//         {
+//             if(arr[i]<k+1)
+//                 k++;
+//             else
+//                 break;
             
+            
+//         }
+//         return k;
+        
+        
+        //optimal binary search
+        int l=0,h=arr.length-1;
+        
+        while(l<=h)
+        {
+            
+            int mid=l+(h-l)/2;
+            
+            int missing=arr[mid]-mid-1;
+            if(missing<k)
+                l=mid+1;
+            else
+                h=mid-1;
             
         }
-        return k;
         
+        /*
+        ans should be arr[h]+(k-missing at h) but h can be -1 if l=0 so
+        arr[h]+k-(arr[h]-h-1)
+        arr[h]+k-arr[h]+h+1
+        k+h+1
+        h+1=l
+        k+l is the ans;
+        
+        */
+        
+        return k+l;
     }
 }
