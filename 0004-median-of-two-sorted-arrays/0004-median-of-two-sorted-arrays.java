5015797a4a797596b403f05e49708ea9b6a98321
sagarrajput2111 <54945125+sagarrajput2111@users.noreply.github.com>
2023-07-12 14:36:00 +0530

    Time: 2 ms (100.00%), Space: 44.6 MB (78.93%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -0,0 +1,38 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
+        //we didi this apply binary search in only small part of the array
+        if(nums2.length<nums1.length)
+            return findMedianSortedArrays(nums2,nums1);
+        
+        int l=0,h=nums1.length;
+        int leftcount=(nums1.length+nums2.length+1)/2;
+        
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            int l1=mid-1>=0?nums1[mid-1]:Integer.MIN_VALUE;
+            int r1=mid<nums1.length?nums1[mid]:Integer.MAX_VALUE;
+            int l2=leftcount-mid-1>=0?nums2[leftcount-mid-1]:Integer.MIN_VALUE;
+            int r2=leftcount-mid<nums2.length?nums2[leftcount-mid]:Integer.MAX_VALUE;
+            
+            if(l1<=r2&&l2<=r1)
+            {
+                if((nums1.length+nums2.length)%2==0)
+                    return (((double)Math.max(l1,l2))+((double)Math.min(r1,r2)))/2.0;
+                else
+                    return (double)Math.max(l1,l2);
+            }
+            else if(l1>r2)
+            {
+                
+                h=mid-1;
+            }
+            else
+            {
+               l=mid+1;
+            }   
+        }
+        return 0.0;
+    }
+}
