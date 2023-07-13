fcc8729117cc4d60736b8b7708a346e8ee7e2358
sagarrajput2111 <54945125+sagarrajput2111@users.noreply.github.com>
2023-07-13 14:17:34 +0530

    Time: 28 ms (5.02%), Space: 44.2 MB (34.42%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,51 @@
+class Solution {
+    public String reverseWords(String s) {
+        
+        //first finding the start index;
+        int start=0;
+        while(start<s.length()&&s.charAt(start)==' ')
+        {
+            start++;
+            //for trailing spaces
+        }
+        
+        //we have found the start
+        
+        int end=start;
+        String temp="";
+        //we are making new strings with reversed words
+        while(end<s.length())
+        {
+            while(end<s.length()&&s.charAt(end)!=' ')
+                end++;
+            //put from end-1 to start in new string
+            for(int i=end-1;i>=start;i--)
+            {
+                temp=temp+s.charAt(i);
+            }
+            temp=temp+" ";
+            while(end<s.length()&&s.charAt(end)==' ')
+                end++;
+            start=end;
+            
+        }
+        s="";
+        System.out.println(temp);
+        for(int i=temp.length()-2;i>=0;i--)
+        {
+            // if(i==temp.length()-1)
+            // {
+            //     while(i>=0&&temp.charAt(i)==' ')
+            //         i--;
+            // }
+            // else
+            // {
+                s=s+temp.charAt(i);
+                
+            // }
+        }
+        
+        return s;
+        
+    }
+}
