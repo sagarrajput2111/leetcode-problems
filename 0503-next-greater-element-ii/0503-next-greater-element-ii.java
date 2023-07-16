2824f2765fd8c8c1975f1b5bf198f49e1c5c2575
sagarrajput2111 <54945125+sagarrajput2111@users.noreply.github.com>
2023-07-16 18:01:30 +0530

    Time: 217 ms (5.06%), Space: 46.1 MB (6.36%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -10,32 +10,49 @@ class Solution {
         
         for(int i = 2 * n - 1; i >= 0; i--)
         {
+            //this one is my ans
             
-            if(stack.isEmpty())
-            {
-                ans[i%n]=-1;
-                stack.add(nums[i%n]);
+//             if(stack.isEmpty())
+//             {
+//                 ans[i%n]=-1;
+//                 stack.add(nums[i%n]);
                 
-            }
+//             }
             
-            else
-            {
-                while(!stack.isEmpty()&&stack.peek()<=nums[i%n])
-                {
-                    stack.pop();
-                }
+//             else
+//             {
+//                 while(!stack.isEmpty()&&stack.peek()<=nums[i%n])
+//                 {
+//                     stack.pop();
+//                 }
                 
-                if(stack.isEmpty())
-                    ans[i%n]=-1;
-                else
-                    ans[i%n]=stack.peek();
-                stack.add(nums[i%n]);
+//                 if(stack.isEmpty())
+//                     ans[i%n]=-1;
+//                 else
+//                     ans[i%n]=stack.peek();
+//                 stack.add(nums[i%n]);
                     
                 
                 
+//             }
+            
+            //this one is from striver
+            
+            while(!stack.isEmpty()&&stack.peek()<=nums[i%n])
+            {
+                stack.pop();
             }
             
-
+            if(i<n)
+            {
+                if(stack.isEmpty()==false)
+                    ans[i]=stack.peek();
+                else
+                    ans[i]=-1;
+            }
+            
+            stack.add(nums[i%n]);
+            
             
         }
         for(int data:ans)
