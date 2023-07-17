29bcfe303d000a5f2ff4198c9017d2b7e2455915
sagarrajput2111 <54945125+sagarrajput2111@users.noreply.github.com>
2023-07-17 19:01:48 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (32.81%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -16,7 +16,7 @@ class Node {
 class Solution {
     public Node copyRandomList(Node head) {
        //with hashmap
-        HashMap<Node,Node> hm=new HashMap<>();
+      /*  HashMap<Node,Node> hm=new HashMap<>();
         Node temp=head;
         while(temp!=null)
         {
@@ -46,6 +46,68 @@ class Solution {
         }
         
         return hm.get(head);
+        */
         
+        //3step process
+        //another approach is create a deep copy of node and add it as next
+        if(head==null)
+            return null;
+        
+        Node temp=head;
+        //creating the dummy node
+        while(temp!=null)
+        {
+            
+           Node node=new Node(temp.val);
+            node.next=temp.next;
+            temp.next=node;
+            temp=temp.next.next;
+        }
+       
+        //putting the random 
+       temp=head;
+        while(temp!=null)
+        {
+            if(temp.random!=null)
+                temp.next.random=temp.random.next;
+            temp=temp.next.next;
+             
+            
+        }
+        
+        
+        
+        // while(head!=null)
+        // {
+        //     System.out.println(head.val);
+        //     head=head.next;
+        // }
+        //now we have to seperate the list
+//         slow=head;
+//         fast=head.next;
+//         Node newHead=head.next;
+//         while(fast!=null)
+//         {
+//             if(fast.next==null)
+//                 break;
+//             slow.next=slow.next.next;
+//             fast.next=fast.next.next;
+            
+//         }
+        
+    Node dummy = new Node(0);
+    Node itr = head;
+    temp = dummy;
+    Node fast=null;
+    while(itr != null) {
+        fast = itr.next.next;
+        temp.next = itr.next;
+        itr.next = fast;
+        temp = temp.next;
+        itr = fast;
+    }
+        
+        
+        return dummy.next;
     }
 }
