6989911e18e9b8eddf3f45607f4397655ae79acd
sagarrajput2111 <54945125+sagarrajput2111@users.noreply.github.com>
2023-07-17 16:23:38 +0530

    Time: 1 ms (95.88%), Space: 44.6 MB (44.45%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -11,40 +11,70 @@ class Solution {
         }
     }
     public int trap(int[] height) {
-        //suffix and prefix sum
+//         //suffix and prefix sum
+//         int n=height.length;
+//         int [] ltr=new int[n],rtl=new int[n];
+//         ltr[0]=height[0];
+//         rtl[0]=height[n-1];
+//         int max1=height[0],max2=height[n-1];
+//         int i=1,j=n-2;
+//         while(i<n)
+//         {
+//             if(max1<height[i])
+//                 max1=height[i];
+//             if(max2<height[j])
+//                 max2=height[j];
+//             ltr[i]=max1;
+//             rtl[i]=max2;
+//             i++;
+//             j--;
+            
+//         }
+        
+//         int sum=0;
+//          // Collections.reverse(Arrays.asList(rtl));
+//         reverse(rtl);
+        
+//         for(int itr=0;itr<n;itr++)
+//         {
+//            System.out.println(ltr[itr]+"   "+rtl[itr]); 
+//         }
+        
+//         for(int itr=1;itr<n-1;itr++)
+//         {
+//             sum+= Math.min(ltr[itr],rtl[itr])-height[itr];
+//         }
+//         return sum;
+        
+        //going to use two ponter approcah
+        
         int n=height.length;
-        int [] ltr=new int[n],rtl=new int[n];
-        ltr[0]=height[0];
-        rtl[0]=height[n-1];
-        int max1=height[0],max2=height[n-1];
-        int i=1,j=n-2;
-        while(i<n)
+        int l=0,r=n-1,lmax=0,rmax=0;
+        int sum=0;
+        while(l<r)
         {
-            if(max1<height[i])
-                max1=height[i];
-            if(max2<height[j])
-                max2=height[j];
-            ltr[i]=max1;
-            rtl[i]=max2;
-            i++;
-            j--;
+            if(height[l]<=height[r])
+            {
+                if(height[l]>lmax)
+                    lmax=height[l];
+                else
+                    sum+=lmax-height[l];
+                l++;
+                    
+            }
+            else
+            {
+                if(height[r]>rmax)
+                    rmax=height[r];
+                else
+                    sum+=rmax-height[r];
+                r--;
+            }
+            
             
         }
-        
-        int sum=0;
-         // Collections.reverse(Arrays.asList(rtl));
-        reverse(rtl);
-        
-        for(int itr=0;itr<n;itr++)
-        {
-           System.out.println(ltr[itr]+"   "+rtl[itr]); 
-        }
-        
-        for(int itr=1;itr<n-1;itr++)
-        {
-            sum+= Math.min(ltr[itr],rtl[itr])-height[itr];
-        }
         return sum;
+        
         
     }
 }
